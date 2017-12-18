384bb688c0e9f4a66e41c9aec82c4337afa75c5a
VijEd <lakshmi2005.d@gmail.com>
2017-12-17 22:44:48 -0800

    Update UserFollower - removed extra spaces

--- MyTwitter/src/main/java/com/ved/ca/mytwitter/domain/UserFollower.java
+++ MyTwitter/src/main/java/com/ved/ca/mytwitter/domain/UserFollower.java
@@ -1,33 +1,24 @@
 package com.ved.ca.mytwitter.domain;
 
 public class UserFollower {
-
+  
   private int userId;
-
   private int followerId;
-
+  
   public int getUserId() {
-
     return userId;
-
   }
 
   public void setUserId(int userId) {
-
     this.userId = userId;
-
   }
 
   public int getFollowerId() {
-
     return followerId;
-
   }
 
   public void setFollowerId(int followerId) {
-
     this.followerId = followerId;
-
   }
 
 }
